d79260547576c4a67b85cf76eacdd14b62e11130
lhhmmiii <lehuuhung30023010@gmail.com>
2026-06-30 13:58:56 +0700

    refactor: remove unused RandomSampler import from gpt1.py

--- models/gpt1.py
+++ models/gpt1.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, RandomSampler
+from torch.utils.data import DataLoader
 
 from dataset import TextDataset
 from train import train_model
