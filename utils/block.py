5a3904223413eed3791343319dd5dcd1f748c8b7
lhhmmiii <lehuuhung30023010@gmail.com>
2026-07-15 11:09:24 +0700

    feat: build logical block from request.

--- utils/block.py
+++ utils/block.py
@@ -1,5 +1,11 @@
 from hashlib import sha256
+import math
 from typing import Optional
+
+from schemas import Request, LogicalBlock
+from paged_attention.kv_cache_manager import KVCacheManager
+
+KV_CACHE_BLOCK_SIZE = 8
 
 def compute_block_hash(
     prev_block_hash: Optional[bytes],
@@ -24,3 +30,35 @@ def compute_block_hash(
         h.update(token.to_bytes(4, byteorder="little", signed=False))
 
     return h.digest()
+
+def build_logical_blocks(request: Request, kv_cache_manager: KVCacheManager) -> Request:
+    """
+    Build logical blocks for a request.
+
+    Args:
+        request: Request need to build logical blocks
+        kv_cache_manager: KV cache manager.
+    
+    Returns:
+        Request with logical blocks.
+    """
+    input_ids = request.input_ids
+    num_blocks = math.ceil(len(input_ids) / KV_CACHE_BLOCK_SIZE)
+    logical_blocks = []
+    prev_block_hash = None
+    for i in range(num_blocks):
+        token_ids = input_ids[i*KV_CACHE_BLOCK_SIZE:(i+1)*KV_CACHE_BLOCK_SIZE]
+        block_hash = compute_block_hash(prev_block_hash, token_ids)
+        physical_block = kv_cache_manager.allocate(block_hash)
+        kv_cache_manager.hash_block(physical_block.block_id, block_hash)
+        logical_block = LogicalBlock(
+            block_idx=i,
+            token_ids=token_ids,
+            block_hash=block_hash,
+            physical_block=physical_block
+        )
+        logical_blocks.append(logical_block)
+        prev_block_hash = block_hash
+
+    request.logical_blocks = logical_blocks
+    return request
